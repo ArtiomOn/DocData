bd2d396e6c7fcd547a7dcc67a0cec58364f00d89
ArtiomOn <artiom.carter.03@mail.ru>
2021-07-19 22:03:28 +0300

    Updated

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+import csv
+import json
+
 import requests
 
-url = "https://docs.google.com/spreadsheets/d/1UVVP4o3e-6wOl4ucP9lPOQQqPoeQWesxlPmb1lK7Qyk/gviz/tq?tqx=out:csv&sheet=0"
+from secrets import *
+
+url = f"https://docs.google.com/spreadsheets/d/{KEY}/gviz/tq?tqx=out:csv&sheet={SHEET}"
 
 r = requests.get(url)
 url_content = r.content
 
-csv_file = open('csv/downloaded.csv', 'wb')
+csv_file = open('data.csv', 'wb')
 csv_file.write(url_content)
+result = {}
+match = {}
+words = ["zero", "one", "two", "few", "many", "other"]
+with open('data.csv', encoding='utf-8', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if str(row['KEY']).split('__')[-1] in words:
+            match = {str(row['KEY']).split('__')[1]: str(row['en'])}
+            result[str(row['KEY'])] = match
+        else:
+            result[row['KEY']] = row['en']
+
+with open('data.json', 'w') as jsonFile:
+    jsonFile.write(json.dumps(result, indent=0))
+
 csv_file.close()
---
+++ secrets.py
@@ -0,0 +1,2 @@
+KEY = "1UVVP4o3e-6wOl4ucP9lPOQQqPoeQWesxlPmb1lK7Qyk"
+SHEET = "0"
