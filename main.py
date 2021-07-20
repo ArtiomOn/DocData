0ca0d5b92c57e76ff74e554692f25c7350b471b5
ArtiomOn <artiom.carter.03@mail.ru>
2021-07-20 12:11:13 +0300

    Updated

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 import csv
 import json
-
 import requests
 
-from secrets import *
+# Docs data
+KEY = '1UVVP4o3e-6wOl4ucP9lPOQQqPoeQWesxlPmb1lK7Qyk'
+SHEET = 0
 
+# Base docs url
 url = f"https://docs.google.com/spreadsheets/d/{KEY}/gviz/tq?tqx=out:csv&sheet={SHEET}"
 
-r = requests.get(url)
-url_content = r.content
+request = requests.get(url)
+url_content = request.content
 
-csv_file = open('data.csv', 'wb')
-csv_file.write(url_content)
-match = {}
-result = {}
-words = ["zero", "one", "two", "few", "many", "other"]
-with open('data.csv', encoding='utf-8', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        if str(row['KEY']).split('__')[-1] in words:
-            match[str(row['KEY']).split('__')[1]] = str(row['en'])
-            result[str(row['KEY']).split('__')[0]] = match
-        else:
-            match = {}
-            result[row['KEY']] = row['en']
 
-with open('data.json', 'w') as jsonFile:
-    jsonFile.write(json.dumps(result, indent=0))
+def key(doc_key, file_name):
+    csv_file = open('csv/data.csv', 'wb')
+    csv_file.write(url_content)
+    result = {}
+    words = ["zero", "one", "two", "few", "many", "other"]
 
-csv_file.close()
+    with open('csv/data.csv', encoding='utf-8', newline='') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            if str(row['KEY']).split('__')[-1] in words:
+                match[str(row['KEY']).split('__')[1]] = str(row[f'{doc_key}'])
+                result[str(row['KEY']).split('__')[0]] = match
+            else:
+                match = {}
+                result[row['KEY']] = row[f'{doc_key}']
+
+        with open(f'json/{file_name}', 'w', encoding='utf-8', newline='') as jsonFile:
+            jsonFile.write(json.dumps(result, indent=0, ensure_ascii=False))
+
+        csv_file.close()
+
+
+key('en', 'data_en.json')
+key('ro', 'data_ro.json')
+key('po', 'data_po.json')
+key('ru', 'data_ru.json')
+key('en-us', 'data_en-rus.json')
--- secrets.py
+++
@@ -1,2 +0,0 @@
-KEY = "1UVVP4o3e-6wOl4ucP9lPOQQqPoeQWesxlPmb1lK7Qyk"
-SHEET = "0"
